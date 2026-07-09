e55df8b9994224a37fc64af62bd0fed9923a1194
ykk17513-abc <ykk17513@gmail.com>
2026-07-09 13:55:28 +0900

    Utilized Trigger for player&wordcards' interaction

---
+++ Assets/Script/WordCard.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WordCard : MonoBehaviour
+{
+    private bool playerNearby = false;
+    //플레이어가 카드 근처에 있는지?
+
+    private void OnTriggerEnter(Collider other) // 플레이어가 카드 영역 안으로 들어오면 실행
+    {
+        if (other.CompareTag("Player")) // 들어온 물체가 플레이어인지 확인
+        {
+            playerNearby = true;
+            Debug.Log("카드 근처!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) //플레이어가 카드 영역 밖으로 나갔을 때 실행
+    {
+        if (other.CompareTag("Player")) // 나간 물체가 플레이어인지 확인
+        {
+            playerNearby = false;
+            Debug.Log("카드에서 멀어짐!");
+        }
+    }
+
+    private void Update()
+    {
+        if (playerNearby && Keyboard.current.enterKey.wasPressedThisFrame)
+        // 플레이어가 카드 근처에 있고 Enter 키를 눌렀을 때 
+        {
+            Debug.Log("카드 선택!");
+        }
+
+    }
+}
+
