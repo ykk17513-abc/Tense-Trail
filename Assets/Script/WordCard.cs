98a0b44f3c3f1e5af847c5ef49958b4cdd6cf859
ykk17513-abc <ykk17513@gmail.com>
2026-07-16 16:06:25 +0900

    learnt concepts

--- Assets/Script/WordCard.cs
+++ Assets/Script/WordCard.cs
@@ -1,21 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class WordCard : MonoBehaviour
+public class WordCard : MonoBehaviour // CLASS PROPERTY, INHERITANCE
 {
-    [SerializeField] private string word;  // 이 카드의 단어
-
+    [SerializeField] private string word;  // 문자열 변수 생성 
     [SerializeField] private MazeManager mazeManager; // GameManager의 MazeManager 연결
+    [SerializeField] private Material wrongMaterial; 
 
-    [SerializeField] private Material wrongMaterial; // 오답일 때 사용할 빨간색 Material
+    private bool playerNearby = false; // 플레이어가 카드 근처에 있는지?, 초기값 설정을 False로 설정 
+    private Renderer cardRenderer; 
 
-
-    private bool playerNearby = false; //플레이어가 카드 근처에 있는지?
-    private Renderer cardRenderer;
-
-    private void Awake()
+    private void Awake() // 게임이 실행될 때, 가장 먼저 실행되는 함수
     {
-        cardRenderer = GetComponent<Renderer>();
+        cardRenderer = GetComponent<Renderer>(); // COLLECTION
     }
 
     private void OnTriggerEnter(Collider other) // 플레이어가 카드 영역 안으로 들어오면 실행
@@ -26,7 +23,6 @@ public class WordCard : MonoBehaviour
             Debug.Log("카드 근처!");
         }
     }
-
     private void OnTriggerExit(Collider other) //플레이어가 카드 영역 밖으로 나갔을 때 실행
     {
         if (other.CompareTag("Player")) // 나간 물체가 플레이어인지 확인
@@ -34,22 +30,17 @@ public class WordCard : MonoBehaviour
             playerNearby = false;
             Debug.Log("카드에서 멀어짐!");
         }
-    }
-
+    } 
     private void Update()
     {
         if (playerNearby && Keyboard.current.enterKey.wasPressedThisFrame)
         // 플레이어가 카드 근처에 있고 Enter 키를 눌렀을 때, MazeManager 호출하기
         {
-            bool isCorrect = mazeManager.CheckAnswer(word);
-            
+            bool isCorrect = mazeManager.CheckAnswer(word); 
             if (!isCorrect)
             {
                 cardRenderer.material = wrongMaterial;
             }
-
         }
-
     }
 }
-
