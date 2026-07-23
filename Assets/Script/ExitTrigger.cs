7e7f554538683c805eec86959f0ae27b91b25801
ykk17513-abc <ykk17513@gmail.com>
2026-07-23 17:43:44 +0900

    updated the ways of giving feedback

--- Assets/Script/ExitTrigger.cs
+++ Assets/Script/ExitTrigger.cs
@@ -5,7 +5,6 @@ public class Exit : MonoBehaviour
 {
     // Take → Took → Taken 완료 여부를 확인
     [SerializeField] private MazeManager mazeManager;
-    // GameView 문구 표시용
     [SerializeField] private TMP_Text feedbackText;
     [SerializeField] private GameObject feedbackPanel;
     private void OnTriggerEnter(Collider other)
@@ -18,15 +17,19 @@ public class Exit : MonoBehaviour
             {
                 float elapsedTime = mazeManager.GetElapsedTime();
                 int wrongCount = mazeManager.GetWrongCount();
+                string feedbackMessage = GetFeedbackMessage(wrongCount);
 
                 if (feedbackText != null)
                 {
                     feedbackPanel.SetActive(true);
                     feedbackText.text = "YOU ESCAPED!" + Environment.NewLine + Environment.NewLine +
-                                        "Time: " + mazeManager.GetElapsedTime() + "s" + Environment.NewLine + 
-                                        "Mistakes: " + mazeManager.GetWrongCount();
+                                        "Time: " + mazeManager.GetElapsedTime() + "s" + Environment.NewLine +
+                                        "Mistakes: " + mazeManager.GetWrongCount() + Environment.NewLine +
+                                        feedbackMessage;
+                   
                 }
                 Debug.Log("You escaped!");
+
             }
             else
             {
@@ -36,6 +39,25 @@ public class Exit : MonoBehaviour
                     feedbackText.text = "Find all correct words first!";
                 }
             }
+        }
+    }
+
+    private string GetFeedbackMessage (int wrongCount)
+    {
+        if (wrongCount == 0)
+        {
+            return "Perfect! Let's go to the next level!";
+        }
+        else if (wrongCount >= 1 && wrongCount <= 2)
+        {
+            return "Good effort! Let's try to improve next time!";
+        }
+        else
+        {
+            return "Review the tense order: Take -> Took -> Taken.";
+        }
+        {
+
         }
     }
 }
